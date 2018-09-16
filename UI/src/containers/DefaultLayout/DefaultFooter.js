79297c89bd33e7297246c552d1e29ee9910b4b23
Eddie <eddie.ren.2013@gmail.com>
2018-09-16 02:25:26 -0400

    renamed my-project to UI

---
+++ UI/src/containers/DefaultLayout/DefaultFooter.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultFooter extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <span><a href="https://coreui.io">HeartBit</a> &copy; MIT License.</span>
+        <span className="ml-auto">ML Powered by <a href="https://hackthenorth.com">TensorFlow</a></span>
+      </React.Fragment>
+    );
+  }
+}
+
+DefaultFooter.propTypes = propTypes;
+DefaultFooter.defaultProps = defaultProps;
+
+export default DefaultFooter;

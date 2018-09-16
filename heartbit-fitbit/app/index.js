65616979b2a3889aa55c73191688e033f06ed528
David <davidzhoucanada@gmail.com>
2018-09-15 23:19:39 -0400

    Add Messages and POST to Fitbit app (not receiving response)

--- heartbit-fitbit/app/index.js
+++ heartbit-fitbit/app/index.js
@@ -1,23 +1,48 @@
 import { HeartRateSensor } from "heart-rate";
 import document from "document";
+// Import the messaging module
+import * as messaging from "messaging";
+
+// Listen for the onopen event
+messaging.peerSocket.onopen = function() {
+  // Ready to send or receive messages
+  console.log("Socket open!");
+}
+
+// Listen for the onerror event
+messaging.peerSocket.onerror = function(err) {
+  // Handle any errors
+  console.log("Connection error: " + err.code + " - " + err.message);
+}
 
 // Create a new instance of the HeartRateSensor object
 let hrm = new HeartRateSensor();
 let lastSent = 0;
-let heartRates = [];
+let heartrates = [];
 
 hrm.onreading = function() {
   // Peek the current sensor values
   console.log("Current heart rate: " + hrm.heartRate);
   document.getElementById("bpm").textContent = hrm.heartRate;
-  heartRates.push(hrm.heartRate)
-  // POST every 5 seconds
+  heartrates.push(hrm.heartRate);
+  // Send every 5 seconds
   if (hrm.timestamp - lastSent >= 5000) {
-    console.log(heartRates);
-    // POST to server
+    console.log("Package: " + heartrates);
+    if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+      // Send data to companion as a message
+      var parcel = {
+        heartrates: heartrates
+      }
+      console.log("Sent to companion!");
+      console.log("Parcel sent:");
+      console.log(parcel);
+      messaging.peerSocket.send(parcel);
+    } else {
+      console.log("Failed to send to companion.");
+    }
     // Check for null/empty/very few elements (implies poor data)
     lastSent = hrm.timestamp;
-    heartRates = [];
+    heartrates = [];
   }
 }
 
--- heartbit-fitbit/companion/index.js
+++ heartbit-fitbit/companion/index.js
@@ -1,5 +1,30 @@
-/*
- * Entry point for the companion app
- */
+import document from "document";
+// Import the messaging module
+import * as messaging from "messaging";
+
+// Listen for the onmessage event
+messaging.peerSocket.onmessage = function(e) {
+  // Output the message to the console
+  console.log("Package received in companion!");
+  var parcel = e.data;
+  console.log("Package: " + JSON.stringify(parcel));
+  var result = postHeartrates(parcel);
+  console.log("Result: " + JSON.stringify(result));
+}
 
 console.log("Companion code started");
+
+function postHeartrates(heartrates)  {
+  fetch(`http://40.76.211.223:3000/fitbit`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(heartrates)
+  })
+  .then(function(res) {
+    console.log("Result: ")
+    return res.json();
+  })
+  .catch(err => console.log('Error: ' + err));
+}

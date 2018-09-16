835ae896958c442c555feb58e72b0e8c5f1d41a9
Eddie <eddie.ren.2013@gmail.com>
2018-09-16 04:04:43 -0400

    socket io client and server working

---
+++ silenus/socket-server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('meme', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(8001, function(){
+  console.log('listening on *:8001');
+});
